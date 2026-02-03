6ad6d6cd20b3354aecb39dc16104ea1c792dd3f1
Eduardo Montilva <edumntg@gmail.com>
2026-02-02 22:47:02 -0400

    a more generic method (still 2x2 system)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use ndarray::prelude::*;
-use ndarray_linalg::solve::Inverse; // Import the trait
+use ndarray_linalg::solve::Inverse;
+use ndarray_rand::RandomExt;
+use rand::distributions::Uniform;
+// Import the trait
 
 // Hello word for newton-raphson using rust
-// Let's solve f(x) = x^2 - 5
-// f'(x) = 2x
 
-fn f(x: &Array2<f64>) -> Array2<f64> {
+fn fx(x: &Array2<f64>) -> Array2<f64> {
     // Return a vector of 2x1 with the two functions
     ndarray::array![
         [x[[0,0]]*x[[0,0]] + x[[1,0]]*x[[1,0]] - 5.0], // f1(x,y) = x^2 + y^2 - 5 = 0
@@ -13,7 +14,7 @@ fn f(x: &Array2<f64>) -> Array2<f64> {
     ] // (2,1)
 }
 
-fn df(x: &Array2<f64>) -> Array2<f64> {
+fn dfx(x: &Array2<f64>) -> Array2<f64> {
     // Return a vector of 2x1 with the two functions
 
     ndarray::array![
@@ -22,26 +23,23 @@ fn df(x: &Array2<f64>) -> Array2<f64> {
     ] // (2,2)
 }
 
-fn main() {
-    let tol: f64 = 1.0e-9;
-    let max_iters: i64 = 20;
-    let mut err: f64 = 1.0e3;
-    let mut current_iters: i64 = 0;
+// Now let's declare here a more generic version
+fn solve(
+    f: fn(&Array2<f64>) -> Array2<f64>,
+    df: fn(&Array2<f64>) -> Array2<f64>,
+    x: &mut Array2<f64>,
+    tol_: Option<f64>,
+    max_iters_: Option<i32>
+) {
+    let tol = tol_.unwrap_or(1e-3);
+    let max_iters = max_iters_.unwrap_or(10);
 
-    // let mut x: Array2<f64> = Array2::from_shape_vec(
-    //     (2,1),
-    //     vec![3.0, 2.0]
-    // ).unwrap(); // seed
-
-    let mut x: Array2<f64> = ndarray::array![
-        [2.0],
-        [-1.0]
-    ];
-
+    let mut err = 1.0e9;
+    let mut current_iter = 0;
     let mut x_old;
 
     // Solving loop
-    while err > tol && current_iters < max_iters {
+    while err > tol && current_iter < max_iters {
         x_old = x.clone();
 
         let fx = f(&x);
@@ -54,12 +52,59 @@ fn main() {
         let step = j_inv.dot(&fx);
 
         // Update x
-        x = &x - &step;
+        *x -= &step;
 
         // Calculate error
-        let diff = &x - &x_old;
+        let diff = &*x - &x_old;
         err = diff.mapv(f64::abs).fold(0.0, |a,b| a.max(*b));
-        current_iters += 1;
-        println!("Iter {} Err {:.8}, x = {:.4}, y = {:.4}", current_iters, err, x[[0,0]], x[[1,0]]);
+        current_iter += 1;
+        println!("Iter {} Err {:.8}, x = {:.4}, y = {:.4}", current_iter, err, x[[0,0]], x[[1,0]]);
     }
+}
+
+fn main() {
+    let tol: f64 = 1.0e-9;
+    let max_iters: i32 = 20;
+
+    let mut x: Array2<f64> = ndarray::array![
+        [2.0],
+        [-1.0]
+    ];
+
+    solve(
+        fx,
+        dfx,
+        &mut x,
+        Option::from(tol),
+        Option::from(max_iters)
+    );
+
+    println!("Sol = {:.4}", x);
+
+
+
+    // let mut x_old;
+    //
+    // // Solving loop
+    // while err > tol && current_iters < max_iters {
+    //     x_old = x.clone();
+    //
+    //     let fx = f(&x);
+    //     let dfx = df(&x); // Jacobian
+    //
+    //     // Inverse of Jacobian
+    //     let j_inv = &dfx.inv().unwrap();
+    //
+    //     // NR Step
+    //     let step = j_inv.dot(&fx);
+    //
+    //     // Update x
+    //     x = &x - &step;
+    //
+    //     // Calculate error
+    //     let diff = &x - &x_old;
+    //     err = diff.mapv(f64::abs).fold(0.0, |a,b| a.max(*b));
+    //     current_iters += 1;
+    //     println!("Iter {} Err {:.8}, x = {:.4}, y = {:.4}", current_iters, err, x[[0,0]], x[[1,0]]);
+    // }
 }
