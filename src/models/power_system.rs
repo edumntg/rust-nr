1f4b1d53059a4b3c628c127d4039ff6ee1789ff5
Eduardo Montilva <c-eduardo.montilva@mrbeastyoutube.com>
2026-03-01 10:14:23 -0400

    nr solver

--- src/models/power_system.rs
+++ src/models/power_system.rs
@@ -1,5 +1,5 @@
 use ndarray::Array2;
-use num_complex::Complex;
+use num_complex::{Complex, ComplexFloat};
 use crate::models::bus::Bus;
 use crate::models::line::Line;
 
@@ -81,5 +81,54 @@ impl PowerSystem {
             );
         }
         println!("\nY-Bus Dimensions: {}x{}", self.ybus.nrows(), self.ybus.ncols());
+    }
+
+    pub fn n(&self) -> i32 {
+        self.buses.len() as i32
+    }
+
+    pub fn prepare(&self) {
+        // This method will be used to set up all poer flow equations
+        // that will be later passed to the solver (i.e newton raphson)
+
+        // Power flow equations
+        //  Pik = (-Bik + gi0)Vi^2 + Vi*Vk*(Gik*cos(thetai-thetak) + Bik*sin(thetai-thetak))
+        //  Qik = (Gik + bi0)Vi^2 - Vi*Vk*(Gik*sin(thetai-thetak) - Bik*cos(thetai-thetak))
+
+        let n: i32 = self.buses.len() as i32; // size of system
+
+        let Gik = self.ybus.mapv(|y| y.re); // Conductance matrix
+        let Bik = self.ybus.mapv(|y| y.im); // Susceptance matrix
+
+        // Build f(x) and df(x). These equations will evaluate the power flow equations (and its derivative)
+        let f: fn(&Array2<f64>) -> Array2<f64> = |x: &Array2<f64>| {
+            // x is a vector of size 2n-1 (V1, V2, ..., Vn-1, theta2, theta3, ..., thetan)
+            let mut fx = Array2::<f64>::zeros((2*n - 1, 1));
+
+            for i in 0..n {
+                let Vi = x[[i as usize, 0]];
+                let thetai = if i == 0 { 0.0 } else { x[[(n + i - 1) as usize, 0]] };
+
+                let mut Pi = 0.0;
+                let mut Qi = 0.0;
+
+                for k in 0..n {
+                    let Vk = x[k as usize, 0];
+                    let thetak = if k == 0 { 0.0 } else { x[[(n + k - 1) as usize, 0]] };
+
+                    Pi += (-Bik[[i as usize, k as usize]] + Gik[[i as usize, k as usize]]) * Vi * Vi
+                        + Vi * Vk * (Gik[[i as usize, k as usize]] * (thetai - thetak).cos() + Bik[[i as usize, k as usize]] * (thetai - thetak).sin());
+
+                    Qi += (Gik[[i as usize, k as usize]] + Bik[[i as usize, k as usize]]) * Vi * Vi
+                        - Vi * Vk * (Gik[[i as usize, k as usize]] * (thetai - thetak).sin() - Bik[[i as usize, k as usize]] * (thetai - thetak).cos());
+                }
+
+                fx[[i as usize, 0]] = Pi - self.buses[i as usize].p_load + self.buses[i as usize].p_gen;
+                fx[[(n + i - 1) as usize, 0]] = Qi - self.buses[i as usize].q_load + self.buses[i as usize].q_gen;
+            }
+
+            fx
+        };
+
     }
 }
---
+++ src/solvers/nr.rs
@@ -0,0 +1,63 @@
+use ndarray::prelude::*;
+use ndarray_linalg::solve::Inverse;
+
+// Generic Newton-Raphson solver
+pub struct NewtonRaphson {
+    pub f: Box<dyn Fn(&Array2<f64>) -> Array2<f64>>,
+    pub df: Box<dyn Fn(&Array2<f64>) -> Array2<f64>>,
+    pub tol: f64,
+    pub max_iters: i32,
+}
+
+impl NewtonRaphson {
+    pub fn new(
+        f: Box<dyn Fn(&Array2<f64>) -> Array2<f64>>,
+        df: Box<dyn Fn(&Array2<f64>) -> Array2<f64>>,
+        tol: Option<f64>,
+        max_iters: Option<i32>,
+    ) -> Self {
+        NewtonRaphson {
+            f,
+            df,
+            tol: tol.unwrap_or(1e-6),
+            max_iters: max_iters.unwrap_or(20),
+        }
+    }
+
+    pub fn solve(&self, x: &mut Array2<f64>) -> bool {
+        let mut err = 1.0e9;
+        let mut current_iter = 0;
+        let mut x_old: Array2<f64>;
+
+        // Solving loop
+        while err > self.tol && current_iter < self.max_iters {
+            x_old = x.clone();
+
+            let fx = (self.f)(x);
+            let dfx = (self.df)(x); // Jacobian
+
+            // Inverse of Jacobian
+            let j_inv = match dfx.inv() {
+                Ok(inv) => inv,
+                Err(_) => {
+                    println!("Jacobian is singular!");
+                    return false;
+                }
+            };
+
+            // NR Step: x_new = x_old - J^-1 * f(x)
+            let step = j_inv.dot(&fx);
+
+            // Update x
+            *x -= &step;
+
+            // Calculate error
+            let diff = x.clone() - x_old;
+            err = diff.mapv(f64::abs).fold(0.0, |a, b| a.max(b));
+            current_iter += 1;
+            println!("Iter {} Err {:.8}", current_iter, err);
+        }
+        
+        err <= self.tol
+    }
+}
